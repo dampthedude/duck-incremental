00a12619ce47bab027e4c75b598c3a6e9a248b7c
dampthedude <dampy5500@gmail.com>
2022-07-01 17:48:56 +1000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -12,7 +12,7 @@ addLayer("d", {
     baseResource: "duck points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 1^"ducks", // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -22,7 +22,7 @@ addLayer("d", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        
+
     ],
     layerShown(){return true}
 })
